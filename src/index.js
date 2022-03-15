5db3fac4626de08a025586a285cf115ed6e39c7b
DamnItAzriel <7pankaj.pawar@gmail.com>
2022-03-15 16:03:49 +0530

    giving unique ids to remove error

--- src/index.js
+++ src/index.js
@@ -4,87 +4,68 @@ import ReactDom from 'react-dom'
 
 //CSS
 import './index.css' 
-// for javaScript file just write./fileName 
-
-
-// Stateless Function Component or Dumb Components
-// Returns JSX
-
-// Nested Tools
-
-//Setup vars
-// const author = 'Hector Garcia';
-// const img = 'https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL604_SR604,400_.jpg';
-// const img2 = 'https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg'; 
-//setup book 1
 const books=[
-{
+{ id: 1,
   img: 'https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL604_SR604,400_.jpg',
   title: 'Ikigai: The Japanese Secret To Long and Happy Life',
   author: 'Hector Garcia'
 },
 {
+  id:2,
   img:'https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg',
   title: 'The Psychology Of Money',
   author: ' Morgan Housel'
+},
+{
+  id: 3,
+  img:'https://images-eu.ssl-images-amazon.com/images/I/814jC+rODgL._AC_UL604_SR604,400_.jpg',
+  title: 'The Subtle Art Of Not Giving F*ck',
+  author: ' Mark Manson'
 }
 ];
 function BookList(){
   return (
     <section className="bookList">
-      {/* Providing props here*/}
+      {/* Providing props here
      <Book
       img={books[0].img}
       title={books[0].title}
       author={books[0].author}
      >
-     {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aliquam, accusamus iusto deleniti libero voluptatibus voluptates tempora! Obcaecati, rem saepe.</p> */}
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aliquam, accusamus iusto deleniti libero voluptatibus voluptates tempora! Obcaecati, rem saepe.</p>
      </Book>
      <Book
       img={books[1].img}
       title={books[1].title}
-      author={books[1].author}
-     />
+      author={books[1].author} */}
+     {/* /> */}
+     {/* using map method to render data from book array to booklist function */}
+     {books.map((book) =>{
+       // structuring 
+      return (
+        <Book key={book.id} book={book}>
+        </Book>
+      )
+       
+     })}
     </section>
   );
 }
-// Trying properties - > props
-
-// const Book = (props) => {
-
-const names = ['john', 'peter', 'susan'];
-const newName = names.map((name) => {
-  console.log(name);
-});
-console.log(newName);
+// const names = ['john', 'peter', 'susan'];
+// const newName = names.map((name) => {
+//   console.log(name);
+// });
+// console.log(newName);
 
 
 const Book = (props) => {
-  // console.log(props);
-  //Object destructuring 
-  // instead of props you could just destructure object in props place itself in function parenthesis
-  const { img, title, author} = props;
+  console.log(props.book)
+  const { img, title, author} = props.book;
   return <article className= 'book'>
-    {/* <img src={props.img} alt="" width="350px"/> */}
-    <img src={img} alt="" width="350px"/>
-    {/* <h1>Ikigai: The Japanese Secret To Long and Happy Life</h1> */}
-    {/* <h1>{props.title}</h1> */}
-    <h1>{title}</h1>
-    {/* <h4>Hector Garcia</h4> */}
-
-    {/* <h4>{props.author}</h4>  */}
-    <h4>{author}</h4>
-    {/* <p>{props.job}</p>
-    <p>{props.title}</p>
-    <p>{props.price}</p> */}
-    {/* {props.children} */}
+      <img src={img} alt="" width="350px"/>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>;
 };
-// const Image = () => ;
-
-
-// const Author = () => <h4 style={{color:'blue', fontSize:'0.75rem', marginTop:'0.25rem'}}>Hector Garcia</h4>;
-
-// const Title = () => <h1>Ikigai: The Japanese Secret To Long and Happy Life</h1>;
 
 ReactDom.render(<BookList></BookList>, document.getElementById('root'));
